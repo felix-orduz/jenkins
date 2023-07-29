926a5211351afe24d092ad85fc2a1693f825ecfe
Felix Ernesto Orduz Grimaldo <felixernestoorduzgrimaldo@Felixs-MacBook-Pro.local>
2023-07-29 07:56:44 -0500

    chore(jdsl.groovy): remove unused code and jobs from the JDSL script
    The code in the JDSL script was cleaned up by removing unused code and jobs that were no longer needed. This improves the readability and maintainability of the script.

--- JDSL/jdsl.groovy
+++ JDSL/jdsl.groovy
@@ -4,27 +4,3 @@ String repo = 'sheehan/grails-example'
 folder(basePath) {
     description 'This example shows basic folder/job creation.'
 }
-
-job("$basePath/grails-example-build") {
-    scm {
-        github repo
-    }
-    triggers {
-        scm 'H/5 * * * *'
-    }
-    steps {
-        grails {
-            useWrapper true
-            targets(['test-app', 'war'])
-        }
-    }
-}
-
-job("$basePath/grails-example-deploy") {
-    parameters {
-        stringParam 'host'
-    }
-    steps {
-        shell 'scp war file; restart...'
-    }
-}
