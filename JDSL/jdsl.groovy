e2a068a32efe86aabcab128f19f7eb88f8f1f5ed
Felix Ernesto Orduz Grimaldo <felixernestoorduzgrimaldo@Felixs-MacBook-Pro.local>
2023-07-30 21:27:20 -0500

    refactor(jdsl.groovy): remove commented out code for configuring multibranch pipeline jobs for organizations and repositories refactor(test.groovy): remove unused test.groovy file

--- JDSL/jdsl.groovy
+++ JDSL/jdsl.groovy
@@ -86,24 +86,3 @@ config["systems"].each { system ->
 
     }
 }
-// config["organizations"].each { org ->
-//   org["repositories"].each { repo ->
-//     multibranchPipelineJob("${org["name"]}_${repo}") {
-//       orphanedItemStrategy {
-//         discardOldItems {
-//           daysToKeep(365) // keep one year
-//         }
-//       }
-
-//       branchSources {
-//         github {
-//           id("${org["name"]}_${repo}")                    // create a unique ID, see below
-//           apiUri('https://git.corp.company.tild/api/v3')  // this is for a GH:E, no need to specify for public GH
-//           scanCredentialsId("${org['user']}_git_token")   // this is the ID of the token in jenkins credentials
-//           repoOwner(org['name'])
-//           repository(repo)
-//         }
-//       }
-//     }
-//   }
-// }
